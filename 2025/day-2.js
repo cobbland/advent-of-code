5b2df90dd48497f0af1d1f9fc8bb728344654139
Jacob Densford <hello@jacobdensford.com>
2025-12-03 11:35:06 -0500

    Removed old code

--- 2025/day-2.js
+++ 2025/day-2.js
@@ -1,45 +1,3 @@
-// // Imports
-// const fs = require('fs');
-// const path = require('path');
-
-// // Get the data
-// const ranges = fs
-//     .readFileSync(path.join(__dirname, 'data/day-2-input.txt'))
-//     .toString()
-//     .split(',');
-
-// function findInvalid(range) {
-//     const [start, end] = range.split('-').map(Number);
-//     let currentNum = start;
-//     let currentPlace = 0;
-//     let hits = 0;
-//     while (currentNum <= end) {
-//         while (currentPlace < currentNum.toString().length) {
-//             const currentNumStr = currentNum.toString();
-//             const currentNumStrFirst = currentNumStr.slice(0, currentPlace);
-//             const currentNumStrLast = currentNumStr.slice(currentPlace);
-//             if (currentNumStrFirst === currentNumStrLast) {
-//                 hits += currentNum;
-//                 console.log(`Hit: ${currentNumStr}`);
-//             }
-//             currentPlace++;
-//         }
-//         currentPlace = 0;
-//         currentNum++;
-//     }
-//     return hits;
-// }
-
-// function findAllInvalid(allRanges = ranges) {
-//     let allHits = 0;
-//     for (const range of allRanges) {
-//         allHits += findInvalid(range);
-//     }
-//     console.log(`Total hits: ${allHits}`);
-// }
-
-// findAllInvalid();
-
 // Imports
 const fs = require('fs');
 const path = require('path');
@@ -57,7 +15,9 @@ function findInvalid(range) {
     let hits = 0;
     while (currentNum <= end) {
         while (currentPlace < currentNum.toString().length) {
-            hits += findInvalidRepeating(currentNum, currentPlace);
+            if (currentNum.toString().length % currentPlace === 0) {
+                hits += findInvalidRepeating(currentNum, currentPlace);
+            }
             currentPlace++;
         }
         currentPlace = 0;
